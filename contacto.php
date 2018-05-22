544dffeaee404cd9fb3149be33e81996f54a79be
dannGarcam <soy.donny@gmail.com>
2018-05-22 09:18:40 -0500

    contacto

---
+++ contacto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contacto</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<!--Cabecera-->
+<div class="barra">
+	<center><h1>Exámen 3</h1></center>
+</div>
+<div class="contenido">
+<h2>Contacto</h2>
+<h3>Queremos saber tu opinión</h3>	
+<a href="soy.donny@gmail.com">Envianos tus comentarios aqui</a>
+</div>
+
+<div class="pie">
+	<a href="index.php">Inicio</a>
+	<a href="contacto.php">Contacto</a>
+	<a href="consulta.php">Consulta de usuarios</a>
+	<a href="creditos.php">Creditos</a>
+</div>
+</body>
+</html>
