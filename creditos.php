82b21d80f7d582f4dc30c7f8a088ff8192af9106
dannGarcam <soy.donny@gmail.com>
2018-05-22 08:51:52 -0500

    formulario y creditos

---
+++ creditos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset='UTF-8'>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Creditos</title>
+</head>
+<body>
+<div class="pagina">
+<div class="encabezado">
+	<h1>Creditos</h1>
+</div>
+
+   <div id="izquierda"style=”padding: 10px; float: left; width: 45%; text-align: justify;”><img src="img/logos.jpeg">
+
+</div>
+
+
+<div class="contenido">
+	<div class="creditos">
+		<h3>Mtro. Tomás Humberto Rubio Pérez </h2>
+		<p>Director</p>
+
+		<h3>Dr. Armando Tomé González</h2>
+		<p>Secretario General</p>
+
+		<h3>Mtro. Balfred Santaella Hinojosa </h2>
+		<p>Jefe del Centro de Informática</p>
+
+		<h3>Daniela Camacho Garduño</h2>
+		<p>Responsable de Diseño y Desarrollo Web</p>
+	</div>
+</div>
+
+<div class="pie">
+	<p>3er parcial</p>
+</div>
+</div>
+</body>
+</html>
